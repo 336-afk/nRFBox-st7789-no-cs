43dcbc186c6073e06d3053d0f47a41e310c7ee95
336-afk <agussahaja336@gmail.com>
2026-05-29 10:38:04 +0700

    Update setting.h

--- nRFBox/setting.h
+++ nRFBox/setting.h
@@ -7,7 +7,14 @@
 #define setting_H
 
 #include <BLEDevice.h>
-#include <U8g2lib.h>
+
+// 🔄 GANTI LIBRARY DISPLAY
+// ❌ HAPUS INI:
+// #include <U8g2lib.h>
+
+// ✅ TAMBAHKAN INI:
+#include <TFT_eSPI.h>
+
 #include <Adafruit_NeoPixel.h>
 #include <EEPROM.h>
 #include <RF24.h>
@@ -17,36 +24,46 @@
 #include <Update.h>
 #include <SPI.h>
 
+// Function declarations (TETAP SAMA)
 void neopixelSetup();
 void neopixelLoop();
-
 void setNeoPixelColour(const std::string& colour);
 void flash(int numberOfFlashes, const std::vector<std::string>& colors, const std::string& finalColour);
 
-extern U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2;
-extern Adafruit_NeoPixel pixels;
+// 🔄 UPDATE EXTERNAL DISPLAY DECLARATION
+// ❌ HAPUS INI:
+// extern U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2;
+
+// ✅ GANTI DENGAN INI:
+extern TFT_eSPI tft;
+
+extern Adafruit_NeoPixel pixels;  // Tetap sama
 
 //extern bool neoPixelActive;
 
 bool neoPixelActive = false;
-uint8_t oledBrightness = 100;
+
+// 🔄 OPTIONAL: Rename variable (bisa tetap 'oledBrightness' kalau mau)
+// uint8_t oledBrightness = 100;
+uint8_t brightness = 100;  // Lebih generic, bisa buat OLED atau TFT
 
 extern RF24 RadioA;
 extern RF24 RadioB;
 extern RF24 RadioC;
 
+// Function declarations (TETAP SAMA)
 void configureNrf(RF24 &radio);
-
 void setRadiosNeutralState();
-
 void setupRadioA();
 void setupRadioB();
 void setupRadioC();
-
 void initAllRadios();
 
+// 🔄 NOTE: Fungsi Str() & CenteredStr() pakai u8g2 di implementasinya
+// Perlu dimodifikasi di file .cpp-nya (setting.cpp / utils.cpp)
 void Str(uint8_t x, uint8_t y, const uint8_t* asciiArray, size_t len);
 void CenteredStr(uint8_t screenWidth, uint8_t y, const uint8_t* asciiArray, size_t len, const uint8_t* font);
+
 void utils();
 void conf();
 
